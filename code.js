59a1bb989a5613588e505e35d00323f2d916487c
Axel Hecht <axel@pike.org>
2016-11-16 17:58:11 +0100

    initial commit

---
+++ code.js
@@ -0,0 +1,60 @@
+/* global URL, fetch, Viz */
+
+const HG = 'https://hg.mozilla.org/try/';
+let logs = new Map();
+
+function loadUser(user, startdate) {
+    let pushesURL = new URL('json-pushes', HG);
+    pushesURL.searchParams.set('user', user);
+    pushesURL.searchParams.set('startdate', startdate);
+    fetch(pushesURL)
+        .then(response => response.json())
+        .then(findAncestor);
+}
+
+function findAncestor(pushes) {
+    let logURL = new URL('json-log', HG);
+    let tips = Object.values(pushes)
+        .map(push => push.changesets[push.changesets.length-1]);
+    logURL.searchParams.set('rev', `ancestor(${tips.join(',')})`);
+    fetch(logURL)
+        .then(response => response.json())
+        .then(function(ancestorLog) {
+            let cs = ancestorLog.entries[0];
+            logs.set(cs.node, cs);
+            let baserev = cs.node;
+            let requests =
+                tips.map(function(tip) {
+                logURL.searchParams.set('rev', `${baserev}::${tip}`);
+                return fetch(logURL)
+                    .then(response => response.json())
+                    .then(function(log) {
+                        log.entries.forEach(cs => logs.set(cs.node, cs));
+                    });
+            });
+            return Promise.all(requests);
+        })
+        .then(renderLogs);
+}
+
+function renderLogs() {
+    let nodes = [], arcs = [];
+    for (let cs of logs.values()) {
+        nodes.push(`"${cs.node}" [ tooltip = "${cs.desc}" label = "${cs.node.slice(0, 12)}" ] ;`);
+        cs.parents
+            .filter(parent => logs.has(parent))
+            .forEach(function (parent) {
+                arcs.push(`"${parent}" -> "${cs.node}" ;`);
+            });
+    }
+    let viz = Viz(`digraph "" {
+  node [shape=box];
+  ${nodes.join("\n")}
+  ${arcs.join("\n")}
+}
+`);
+    document.getElementById('output').innerHTML = viz;
+}
+
+let params = new URL(document.location).searchParams;
+loadUser(params.get('user'), `${params.get('days')} days before now`);
