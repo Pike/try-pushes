f74a570c42bdcdcb28286ecb76bf906ddf6493fc
Axel Hecht <axel@pike.org>
2016-11-17 13:28:52 +0100

    add a link to talos-compare, with implicit orderig, though

--- code.js
+++ code.js
@@ -3,12 +3,19 @@
 const HG = 'https://hg.mozilla.org/try/';
 let logs = new Map();
 
+function addProgress(closure) {
+    document.getElementById('output').textContent += '.';
+    return closure;
+}
+
 function loadUser(user, startdate) {
+    if (!user || !startdate) return;
     let pushesURL = new URL('json-pushes', HG);
     pushesURL.searchParams.set('user', user);
     pushesURL.searchParams.set('startdate', startdate);
     fetch(pushesURL)
         .then(response => response.json())
+        .then(addProgress)
         .then(findAncestor);
 }
 
@@ -19,6 +26,7 @@ function findAncestor(pushes) {
     logURL.searchParams.set('rev', `ancestor(${tips.join(',')})`);
     fetch(logURL)
         .then(response => response.json())
+        .then(addProgress)
         .then(function(ancestorLog) {
             let cs = ancestorLog.entries[0];
             logs.set(cs.node, cs);
@@ -28,6 +36,7 @@ function findAncestor(pushes) {
                 logURL.searchParams.set('rev', `${baserev}::${tip}`);
                 return fetch(logURL)
                     .then(response => response.json())
+                    .then(addProgress)
                     .then(function(log) {
                         log.entries.forEach(cs => logs.set(cs.node, cs));
                     });
@@ -57,7 +66,27 @@ function renderLogs() {
   ${arcs.join("\n")}
 }
 `);
-    document.getElementById('output').innerHTML = viz;
+    let output = document.getElementById('output');
+    output.innerHTML = viz;
+    let try_nodes = Array.from(output.querySelectorAll('text'))
+        .filter(text_node => /^[0-9a-g]{12}$/.test(text_node.textContent));
+    try_nodes.forEach(function(try_node) {
+        try_node.parentNode.onclick = function() {
+            try_node.classList.toggle('selected');
+            updateTalosCompare();
+        };
+    });
+}
+
+function updateTalosCompare() {
+    let talos_compare = new URL('https://pike.github.io/talos-compare/');
+    Array.from(
+            document.querySelectorAll('.selected'),
+            tn => tn.textContent)
+        .forEach(rev => talos_compare.searchParams.append('revision',rev));
+    let talos_link = document.getElementById('talos-link');
+    talos_link.href = talos_compare;
+    talos_link.disabled = false;
 }
 
 let params = new URL(document.location).searchParams;
